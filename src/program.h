7a9cac0e9fe3ec90ee3ee2ceff1d8bc33b3d05e7
YangTuanAnh <yangtuananh2003@gmail.com>
2022-10-24 16:09:10 +0700

    add: Initialize template file for Raylib, Raygui

---
+++ src/program.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <raylib.h>
+#include "../include/raygui.h"
+
+const int WIDTH = 1200, HEIGHT = 600;
+class Program
+{
+private:
+    Rectangle rec_top{0, 0, 1200, 100};
+
+public:
+    Program() {}
+    ~Program() {}
+    void run() {}
+};
