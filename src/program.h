cf6179ee53c2a6075c6142f73949a1da0738d42d
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2022-12-12 22:14:01 +0700

    Movement of Player as block

--- src/program.h
+++ src/program.h
@@ -1,15 +1,22 @@
 #pragma once
+#include "Game.h"
 #include <raylib.h>
 #include "../include/raygui.h"
 
-const int WIDTH = 1200, HEIGHT = 600;
+//const int WIDTH = 1200, HEIGHT = 600;
 class Program
 {
 private:
     Rectangle rec_top{0, 0, 1200, 100};
+    Player * mPlayer;
 
 public:
-    Program() {}
+    Program() {
+        mPlayer = new Player(0,0,0);
+    }
     ~Program() {}
-    void run() {}
+    void run() {
+        mPlayer->UpdatePlayer();
+        mPlayer->DrawPlayer();
+    }
 };
