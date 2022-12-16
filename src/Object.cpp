6e03b5621e9224b18b57403b6a23513aee2849e0
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2022-12-16 15:25:09 +0700

    Revert "merge form hao/object"
    This reverts commit 5e76e211d0ca98f37ba3a7a53dcd4cc8959232ee, reversing
    changes made to e46ee018e1f09719bcaa983086c3fcb2b1c17832.

--- src/Object.cpp
+++ src/Object.cpp
@@ -1,137 +1,37 @@
 #include "Object.h"
 #include "Player.hpp"
-#include "program.h"
 
-float Object::getX(){
+int Object::getX(){
     return this->X;
 }
-float Object::getY(){
+int Object::getY(){
     return this->Y;
 }
-
-void Dinausor::draw() {
-    Texture2D texture = mTextures->get(Textures::Dinasor);  // get texture
-    if (this->direct == 1) {
-        Vector2 pos = { this->X, this->Y };
-        DrawTextureV(texture, pos, WHITE);
-    }
-    else {  // flip texture to the suitable direction
-        Image inverseDirect = ImageCopy(LoadImageFromTexture(texture));
-        ImageFlipHorizontal(&inverseDirect);
-        Texture2D inverseTexture = LoadTextureFromImage(inverseDirect);
-        // Color* pixels = LoadImageColors(inverseDirect);
-        // UpdateTexture(inverseTexture, pixels);
-        // UnloadImageColors(pixels);
-
-        Vector2 pos = { this->X, this->Y };
-        DrawTextureV(inverseTexture, pos, WHITE);
-    }
-}
-
 Dinausor::~Dinausor(){
     this->direct = 0;
     this->X = 0;
     this->Y = 0;
 }
-
-void Car::draw() {
-    Texture2D texture = mTextures->get(Textures::Car);  // get texture
-    if (this->direct == 1) {
-        Vector2 pos = { this->X, this->Y };
-        DrawTextureV(texture, pos, WHITE);
-    }
-    else {  // flip texture to the suitable direction
-        Image inverseDirect = ImageCopy(LoadImageFromTexture(texture));
-        ImageFlipHorizontal(&inverseDirect);
-        Texture2D inverseTexture = LoadTextureFromImage(inverseDirect);
-        // Color* pixels = LoadImageColors(inverseDirect);
-        // UpdateTexture(inverseTexture, pixels);
-        // UnloadImageColors(pixels);
-
-        Vector2 pos = { this->X, this->Y };
-        DrawTextureV(inverseTexture, pos, WHITE);
-    }
-}
-
 Car::~Car(){
     this->direct = 0;
     this->X = 0;
     this->Y = 0;
 }
-
-void Truck::draw() {
-    Texture2D texture = mTextures->get(Textures::Truck);  // get texture
-    if (this->direct == 1) {
-        Vector2 pos = { this->X, this->Y };
-        DrawTextureV(texture, pos, WHITE);
-    }
-    else {  // flip texture to the suitable direction
-        Image inverseDirect = ImageCopy(LoadImageFromTexture(texture));
-        ImageFlipHorizontal(&inverseDirect);
-        Texture2D inverseTexture = LoadTextureFromImage(inverseDirect);
-        // Color* pixels = LoadImageColors(inverseDirect);
-        // UpdateTexture(inverseTexture, pixels);
-        // UnloadImageColors(pixels);
-
-        Vector2 pos = { this->X, this->Y };
-        DrawTextureV(inverseTexture, pos, WHITE);
-    }
-}
-
 Truck::~Truck(){
     this->direct = 0;
     this->X = 0;
     this->Y = 0;
 }
-
-void Bird::draw() {
-    Texture2D texture = mTextures->get(Textures::Bird);  // get texture
-    if (this->direct == 1) {
-        Vector2 pos = { this->X, this->Y };
-        DrawTextureV(texture, pos, WHITE);
-    }
-    else {  // flip texture to the suitable direction
-        Image inverseDirect = ImageCopy(LoadImageFromTexture(texture));
-        ImageFlipHorizontal(&inverseDirect);
-        Texture2D inverseTexture = LoadTextureFromImage(inverseDirect);
-        // Color* pixels = LoadImageColors(inverseDirect);
-        // UpdateTexture(inverseTexture, pixels);
-        // UnloadImageColors(pixels);
-
-        Vector2 pos = { this->X, this->Y };
-        DrawTextureV(inverseTexture, pos, WHITE);
-    }
-}
-
 Bird::~Bird(){
     this->direct = 0;
     this->X = 0;
     this->Y = 0;
 
 }
-
 Object::~Object(){
     this->direct = 0;
     this->X = 0;
     this->Y = 0;
-}
-
-void Obstacle::draw() {
-    Texture2D texture = mTextures->get(Textures::Obstacle);  // get texture
-    if (this->direct == 1) {
-        Vector2 pos = { this->X, this->Y };
-        DrawTextureV(texture, pos, WHITE);
-    }
-    else {  // flip texture to the suitable direction
-        Image inverseDirect = ImageCopy(LoadImageFromTexture(texture));
-        ImageFlipHorizontal(&inverseDirect);
-        Texture2D inverseTexture = LoadTextureFromImage(inverseDirect);
-        // Color* pixels = LoadImageColors(inverseDirect);
-        // UpdateTexture(inverseTexture, pixels);
-        // UnloadImageColors(pixels);
-        Vector2 pos = { this->X, this->Y };
-        DrawTextureV(inverseTexture, pos, WHITE);
-    }
 }
 Obstacle::~Obstacle(){
     this->direct = 0;
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,12 @@
 #include "program.h"
-#include "Object.h"
 
 int main()
 {
     InitWindow(WIDTH, HEIGHT, "Crash Course");
     SetTargetFPS(60);
     Program Program;
-    Texture2D texture = LoadTexture("../image/animal_dinosaur.png");
-
-    Image inverseDirect = ImageCopy(LoadImageFromTexture(texture));
-    ImageFlipHorizontal(&inverseDirect);
-    Texture2D inverseTexture = LoadTextureFromImage(inverseDirect);
-    Vector2 pos = { 10.0f, 10.0f };
-
-    while (!WindowShouldClose()) {
-        // Color* pixels = LoadImageColors(inverseDirect);
-        // UpdateTexture(inverseTexture, pixels);
-        // UnloadImageColors(pixels);
-
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-        // DrawTexture(inverseTexture, WIDTH/2 - texture.width/2, HEIGHT/2 - texture.height/2, WHITE);
-        DrawTextureV(inverseTexture, pos, WHITE);
-        pos.x += 2.0f;
-
-        EndDrawing();
-    }
-        //Program.run();
+    while (!WindowShouldClose())
+        Program.run();
     CloseWindow();
     return 0;
 }
