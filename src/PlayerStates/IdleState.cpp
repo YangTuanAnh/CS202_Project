aeb1bf3ba3ea88beadfb9eb8b8cb6b3d1ea6a00c
Nguyen Nhu Hoai Nam <hoainamnguyennhu7@gmail.com>
2022-12-17 10:50:14 +0700

    refactor input hadling

--- src/PlayerStates/IdleState.cpp
+++ src/PlayerStates/IdleState.cpp
@@ -6,22 +6,26 @@
 IdleState::IdleState(Player& player) : PlayerState(player, PlayerStates::Idle) {}
 
 void IdleState::update(float dt) {
-    Directions::ID direction;
-    if (IsKeyPressed(KEY_W)) {
+    int keyPressed = GetKeyPressed();
+    if (keyPressed == KEY_W || keyPressed == KEY_A || keyPressed == KEY_S || keyPressed == KEY_D) {
+        Directions::ID direction;
         player.setState(PlayerStates::Jumping);
-        direction = Directions::Up;
-        player.mState->setTarget(Vector2{player.getX(), player.getY()}, direction);
-    } else if (IsKeyPressed(KEY_S)) {
-        player.setState(PlayerStates::Jumping);
-        direction = Directions::Down;
-        player.mState->setTarget(Vector2{player.getX(), player.getY()}, direction);
-    } else if (IsKeyPressed(KEY_A)) {
-        player.setState(PlayerStates::Jumping);
-        direction = Directions::Left;
-        player.mState->setTarget(Vector2{player.getX(), player.getY()}, direction);
-    } else if (IsKeyPressed(KEY_D)) {
-        player.setState(PlayerStates::Jumping);
-        direction = Directions::Right;
+        switch (keyPressed) {
+        case KEY_W:
+            direction = Directions::Up;
+            break;
+        case KEY_S:
+            direction = Directions::Down;
+            break;
+        case KEY_A:
+            direction = Directions::Left;
+            break;
+        case KEY_D:
+            direction = Directions::Right;
+            break;
+        default:
+            break;
+        }
         player.mState->setTarget(Vector2{player.getX(), player.getY()}, direction);
     }
 }
