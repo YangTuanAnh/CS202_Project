d782af8fad7d68c12e63b9845f6ef5a666f2048b
YangTuanAnh <yangtuananh2003@gmail.com>
2022-12-20 12:54:18 +0700

    Change dir for GameStates to hpp extension

--- src/Program.cpp
+++ src/Program.cpp
@@ -1,10 +1,6 @@
 #include "Program.h"
-<<<<<<< HEAD
 #include "States/MenuState.h"
-#include "States/GameState.h"
-=======
 #include "States/GameState.hpp"
->>>>>>> 9f06b4a67da96c6084bef4dc47dfcfc4f4e52d81
 #include "States/PausedState.h"
 #include "PlayerStates/IdleState.h"
 #include "PlayerStates/JumpingState.h"
@@ -36,16 +32,9 @@ Program::~Program() {
 
 void Program::run() {
     const char msg1[50] = "Game Over!";
-<<<<<<< HEAD
-    //Font font1 = LoadFont("resources/custom_mecha.png");
-    Font font1 = GetFontDefault();
-    Vector2 fontPosition1 = { WIDTH/2.0f - MeasureTextEx(font1, msg1, (float)font1.baseSize, -3).x/2,
-                              HEIGHT/2.0f - font1.baseSize/2.0f - 80.0f };
-=======
     // Font font1 = LoadFont("resources/custom_mecha.png");
     // Vector2 fontPosition1 = { SCREEN_WIDTH/2.0f - MeasureTextEx(font1, msg1, (float)font1.baseSize, -3).x/2,
     //                           SCREEN_HEIGHT/2.0f - font1.baseSize/2.0f - 80.0f };
->>>>>>> 9f06b4a67da96c6084bef4dc47dfcfc4f4e52d81
     Vector2 pos = {this->mPlayer->getX(),this->mPlayer->getY()};
     Rectangle rec = {0.0f,0.0f,39.0f,39.0f};//base on object size
     BeginDrawing();
