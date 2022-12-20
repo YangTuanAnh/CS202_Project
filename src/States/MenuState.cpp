9342428a215f78c0819e890c3b6b987ed67fee79
YangTuanAnh <yangtuananh2003@gmail.com>
2022-12-20 14:19:34 +0700

    Add Pause State

--- src/States/MenuState.cpp
+++ src/States/MenuState.cpp
@@ -1,37 +1,26 @@
 #include "MenuState.h"
 MenuState::MenuState(StateStack *stack, Context context) : State(stack, context) {
-    std::string playOption = "Play";
-    std::string exitOption = "Exit";
-    //playOption.setFont(font);
-    //DrawText(playOption.c_str(), recMenu[0].x, recMenu[1].y, 20, BLACK);
-    //centerOrigin(playOption);
-    //playOption.setPosition(context.window->getView().getSize() / 2.f);
-    mOptions.push_back(playOption);
-    mOptions.push_back(exitOption);
+    mOptions.push_back("Start Game");
+    mOptions.push_back("Load Game");
+    mOptions.push_back("Settings");
+    mOptions.push_back("Exit");
 }
 
 void MenuState::draw() {
     ClearBackground(RAYWHITE);
     int len = mOptions.size();
     for (int i=0; i<len; i++)
-        //GuiButton(recMenu[i], mOptions[i].c_str());
         DrawText(mOptions[i].c_str(), 600-MeasureText(mOptions[i].c_str(), 20)/2, recMenu[i].y+15, 20, BLACK);
+    for (int i = 0; i < len; i++)
+        if (CheckCollisionPointRec(GetMousePosition(), recMenu[i]))
+            DrawRectangleRec(recMenu[i], {0, 0, 0, 255/4});
 }
 bool MenuState::update(float dt) {
     bool updatePrevState = false;
-    // if (IsKeyPressed(KEY_TAB)) {
-    //     requestStackPush(States::Pause);
-    //     return updatePrevState;
-    // }
-    // player->update(dt);
-    // return updatePrevState;
-    for (int i = 0; i < mOptions.size(); i++)
-    {
-        if (CheckCollisionPointRec(GetMousePosition(), recMenu[i]))
-        {
-            DrawRectangleRec(recMenu[i], {0, 0, 0, 255/4});
-            cerr << mOptions[i] << '\n';
-        }
+    if (IsKeyPressed(KEY_ESCAPE)) {
+        requestStackPop();
+        CloseWindow();
+        return updatePrevState;
     }
 
     if (IsMouseButtonDown(0)) {
--- src/States/PausedState.cpp
+++ src/States/PausedState.cpp
@@ -4,10 +4,23 @@
 
 PausedState::PausedState(StateStack* stack, Context context) : State(stack, context) {
     mBackgroundTexture = &context.textures->get(Textures::PausedBackground);
+    mOptions.push_back("Resume");
+    mOptions.push_back("Return to Main menu");
 }
 
 void PausedState::draw() {
     DrawTexture(*mBackgroundTexture, 0, 0, WHITE);
+    int len = mOptions.size();
+    for (int i=0; i<len; i++)
+        DrawText(mOptions[i].c_str(), recButton[i].x + 150-MeasureText(mOptions[i].c_str(), 20)/2, recButton[i].y+15, 20, BLACK);
+    for (int i = 0; i < mOptions.size(); i++)
+    {
+        if (CheckCollisionPointRec(GetMousePosition(), recButton[i]))
+        {
+            DrawRectangleRec(recButton[i], {0, 0, 0, 255/4});
+            cerr << mOptions[i] << '\n';
+        }
+    }
 }
 
 bool PausedState::update(float dt) {
@@ -15,6 +28,18 @@ bool PausedState::update(float dt) {
     if (IsKeyPressed(KEY_TAB)) {
         requestStackPop();
         return updatePrevState;
+    }
+
+    if (IsMouseButtonDown(0)) {
+        if (CheckCollisionPointRec(GetMousePosition(), recButton[Resume])) {
+            requestStackPop();
+            return updatePrevState;
+        }
+        if (CheckCollisionPointRec(GetMousePosition(), recButton[Return])) {
+            requestStackPop();
+            requestStackPop();
+            return updatePrevState;
+        }
     }
     return updatePrevState;
 }
