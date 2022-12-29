17669737337ec7510521128b5008923f57324a62
Nguyen Nhu Hoai Nam <hoainamnguyennhu7@gmail.com>
2022-12-29 08:51:29 +0700

    shaking camera

--- src/Camera.h
+++ src/Camera.h
@@ -8,6 +8,9 @@
 class CustomCamera : public Camera2D {
 private:
     Player *player;
+    // non-isometric position
+    Vector2 pos;
+    float velocity = -20.0f;
 public:
     CustomCamera(Player *player);
     void getCamera();
